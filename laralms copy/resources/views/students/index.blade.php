520c34755ac8685f75721271c534a77fc1d85e9a
Tianrui Zhu <weltandpuzzle@gmail.com>
2025-04-01 17:49:21 -0400

    week11-laravel edit and create

---
+++ laralms copy/resources/views/students/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+@section('content')
+
+<h1>All Students</h1>
+    <ul>
+        @foreach ( $students as $student)
+        <li>{{ $student -> fname}} {{ $student -> lname}} |
+            <a href="{{ route('students.edit', $student -> id )}}">Edit</a>
+        </li>
+        @endforeach
+    </ul>
+@endsection
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    All Students
+     @foreach ( $students as $student)
+        {{ $student -> fname}} <br>
+    @endforeach
+</body>
+</html>
+-->
